88d101ef5692e73065030f91a0f9c079daecffb8
Daniel Bendel <daniel.bendel@gmail.com>
2016-09-28 13:14:18 +0200

    module load order experiments

--- src/main.ts
+++ src/main.ts
@@ -10,19 +10,37 @@ import environment from './environment';
 });
 
 export function configure(aurelia: Aurelia) {
-  aurelia.use
-    .standardConfiguration()
-    .feature('resources');
+  // aurelia.use
+  //   .standardConfiguration()
+  //   .feature('resources');
 
-  if (environment.debug) {
-    aurelia.use.developmentLogging();
-  }
+  // if (environment.debug) {
+  //   aurelia.use.developmentLogging();
+  // }
 
-  if (environment.testing) {
-    aurelia.use.plugin('aurelia-testing');
-  }
+  // if (environment.testing) {
+  //   aurelia.use.plugin('aurelia-testing');
+  // }
 
-  aurelia.use.plugin('aurelia-materialize-bridge', b => b.useAll());
+  // aurelia.use.plugin('aurelia-materialize-bridge', b => b.useAll());
 
-  aurelia.start().then(() => aurelia.setRoot());
+  // aurelia.start().then(() => aurelia.setRoot());
+  return aurelia.loader.loadModule('jquery').then(() => {
+    return aurelia.loader.loadModule('materialize-css').then(() => {
+      aurelia.use
+        .standardConfiguration()
+        .feature('resources');
+
+      if (environment.debug) {
+        aurelia.use.developmentLogging();
+      }
+
+      if (environment.testing) {
+        aurelia.use.plugin('aurelia-testing');
+      }
+      aurelia.use.plugin('aurelia-materialize-bridge', b => b.useAll());
+
+      return aurelia.start().then(() => aurelia.setRoot());
+    });
+  });
 }
